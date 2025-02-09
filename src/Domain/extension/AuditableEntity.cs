81ab7812085de7bbf623d884a29c1e0c4178b832
yojana-hawaii <yojana.hawaii@gmail.com>
2025-02-08 20:40:24 -1000

    override SaveChanges and SaveChangesAsync for auditable entity

--- src/Domain/extension/AuditableEntity.cs
+++ src/Domain/extension/AuditableEntity.cs
@@ -3,12 +3,19 @@ using System.Runtime.CompilerServices;
 
 namespace Domain.extension;
 
-public class AuditableEntity
+public class AuditableEntity : IAuditable
 {
+    [ScaffoldColumn(false)]
     [StringLength(100)]
     public string? CreatedBy { get; set; }
+
     [StringLength(100)]
+    [ScaffoldColumn(false)]
     public string? ModifiedBy { get; set; }
+
+    [ScaffoldColumn(false)]
     public DateTime CreatedDate { get; set; }
+
+    [ScaffoldColumn(false)]
     public DateTime ModifiedDate { get; set; }
 }
--- src/Infrastructure/dbcontext/DirectoryContext.cs
+++ src/Infrastructure/dbcontext/DirectoryContext.cs
@@ -1,4 +1,6 @@
 ﻿using Domain.directory;
+using Domain.extension;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
@@ -6,6 +8,18 @@ namespace Infrastructure.dbcontext;
 
 public class DirectoryContext : DbContext
 {
+    private readonly IHttpContextAccessor? _httpContextAccessor;
+    public string? LoggedInUser { get; private set; }
+
+    public DirectoryContext(DbContextOptions<DirectoryContext> options, IHttpContextAccessor httpContextAccessor) : base(options)
+    {
+        _httpContextAccessor = httpContextAccessor;
+        if (_httpContextAccessor.HttpContext != null)
+        {
+            LoggedInUser = _httpContextAccessor.HttpContext.User.Identity?.Name;
+            LoggedInUser = LoggedInUser ?? "Unknown";
+        }
+    }
     public DirectoryContext(DbContextOptions<DirectoryContext> options) : base(options) { }
 
 
@@ -19,5 +33,41 @@ public class DirectoryContext : DbContext
     {
         //apply all fluent api configuration to entity using reflection
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+    public override int SaveChanges(bool acceptAllChangesOnSuccess)
+    {
+        OnBeforeSaving();
+        return base.SaveChanges(acceptAllChangesOnSuccess);
+    }
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken)
+    {
+        OnBeforeSaving();
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess,cancellationToken);
+    }
+
+    private void OnBeforeSaving()
+    {
+        var entities = ChangeTracker.Entries();
+        foreach (var entity in entities)
+        {
+            if(entity.Entity is IAuditable trackable)
+            {
+                var now = DateTime.UtcNow;
+                switch (entity.State)
+                {
+                    case EntityState.Modified:
+                        trackable.ModifiedBy = LoggedInUser;
+                        trackable.ModifiedDate = now;
+                        break;
+                    case EntityState.Added:
+                        trackable.ModifiedBy = LoggedInUser;
+                        trackable.ModifiedDate = now;
+                        trackable.CreatedBy = LoggedInUser;
+                        trackable.CreatedDate = now;
+                        break;
+                }
+            }
+        }
     }
 }
