da5c2615c906c550b50783a47a5265b7f056e806
yojana-hawaii <yojana.hawaii@gmail.com>
2025-03-02 12:16:20 -1000

    manager rename migration

--- src/Infrastructure/DirectoryMigration/20250302213155_RenameManager.Designer.cs
+++ src/Infrastructure/DirectoryMigration/20250302213155_RenameManager.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.dbcontext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.DirectoryMigration
 {
     [DbContext(typeof(DirectoryContext))]
-    partial class DirectoryContextModelSnapshot : ModelSnapshot
+    [Migration("20250302213155_RenameManager")]
+    partial class RenameManager
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
